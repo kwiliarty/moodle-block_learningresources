a203a948da4b475830015a76f5da5016af9442c2
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2013-07-12 12:42:21 -0400

    able to save settings. have to enter links as text :-(

--- settings.php
+++ settings.php
@@ -1,4 +1,6 @@
 <?php
 
-$settings->add(new admin_setting_configcheckbox('learningresources/new_window', 'Open in an new window',
+$settings->add(new admin_setting_configcheckbox('learningresources/new_window', 'Open links in an new window',
                         '', 'learningresources', 1));
+
+$settings->add(new admin_setting_configtextarea('learningresources/link_list', 'List of learning resources', 'display text|url|show or hide', '', PARAM_RAW, 120, 20));
