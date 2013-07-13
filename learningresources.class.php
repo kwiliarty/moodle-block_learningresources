cbfe51654344aa0e7caab08f05c8cd9980537484
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2013-07-13 09:49:56 -0400

    use html_writer to format links

--- learningresources.class.php
+++ learningresources.class.php
@@ -1,20 +1,5 @@
 <?php
 
-//$resourcearray = array(
-//    'lib_home'     => 'http://www.smith.edu/libraries',
-//    'research'     => 'http://www.smith.edu/libraries/research/',
-//    'apa_citation' => 'http://libguides.smith.edu/content.php?pid=31706&sid=231793#678041',
-//    'services'     => 'http://www.smith.edu/libraries/services/ssw/',
-//    'sw_resources' => 'http://www.smith.edu/libraries/research/class/sswspecialized.htm',
-//    'fc_catalog'   => 'http://fcaw.library.umass.edu:8991/F/',
-//    'journals'     => 'http://www.smith.edu/libraries/research/article.html',
-//    'cite_guides'  => 'http://libguides.smith.edu/citation',
-//    'refworks'     => 'http://libguides.smith.edu/refworks',
-//    'ssw_subjects' => 'http://www.smith.edu/libraries/research/subject/socialwork.htm',
-//    'lib_help'     => 'http://www.smith.edu/libraries/help/',
-//    'luna_db'      => 'http://www.smith.edu/imaging/luna.htm'
-//);
-
 class lr_list {
 
     public $lr_array = array();
@@ -38,7 +23,8 @@ class lr_list {
             // Not sure why, but a simple test for equality failed, maybe line ending?
             // The regular expression test gets the desired behavior
             if (!(preg_match('/show/', $row[2]))) { continue; }
-            $this->html_array[$key] = "<a href='$row[1]'>$row[0]</a>"; 
+            //$this->html_array[$key] = "<a href='$row[1]'>$row[0]</a>"; 
+            $this->html_array[$key] = html_writer::link($row[1], $row[0]); 
         }
         return $this->html_array;
     }
