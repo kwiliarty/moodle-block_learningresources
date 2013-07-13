2506bb17629f4beceac6b5ba22a7eb449cc35f42
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2013-07-13 06:40:37 -0400

    now displaying default list correctly

--- block_learningresources.php
+++ block_learningresources.php
@@ -31,9 +31,7 @@ class block_learningresources extends block_base {
         $default_lr_array = new lr_list($raw_defaults);
 
         $this->content = new stdClass;
-        $this->content->text = 'Learning resources';
-        $this->content->text .= $default_lr_array->raw_list;;
-        $this->content->text .= $default_lr_array->get_html_list();
+        $this->content->text = $default_lr_array->get_html_list();
 
         return $this->content;
     }
