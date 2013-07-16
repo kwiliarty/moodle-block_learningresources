1f5b832077681af9ba368040577923cf1bf811ab
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2013-07-16 06:35:39 -0400

    use html_writer for link on edit_form, comments

--- edit_form.php
+++ edit_form.php
@@ -13,19 +13,14 @@ class block_learningresources_edit_form extends block_edit_form { /* creates the
         // title for the checkbox section
         $mform->addElement('header', 'configheader', get_string('blocksettings', 'block_learningresources'));
 
-        // loop through the default array
-        foreach ($lr_array as $lr) {
-            // format a link to the resource
-            $anchor = "<a href='" . $lr['url'] . "' target='_blank' >" . $lr['text'] . "</a>";
-            // variable to hold the specific fieldname
-            $fieldname = "config_" . $lr['id'];
-            // add a checkbox
-            // Moodle will apply the preferences if they have been set
-            $mform->addElement('advcheckbox', 
+        foreach ($lr_array as $lr) { /* loop through the default resources */
+            $anchor = html_writer::link($lr['url'], $lr['text'], array('target'=>'_blank')); /* link to resource */
+            $fieldname = "config_" . $lr['id']; /* fieldname for checkbox */
+            $mform->addElement('advcheckbox', /* moodle applies preferences if they have been set already */
                                $fieldname,
                                $lr['id'], /* displays before the checkbox */
-                               $anchor,
-                               null,
+                               $anchor, /* displays after checkbox */
+                               null, /* potential group for checkbox controller */
                                array('hide', 'show')); /* assigns values to "off" and "on" */
             $mform->setDefault($fieldname, $lr['show']); /* use defaults if no instance config */
         }
