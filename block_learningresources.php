ca1093b4c99eae8ecbc9f92a301497d32028df71
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2013-07-16 06:15:40 -0400

    commenting main file

--- block_learningresources.php
+++ block_learningresources.php
@@ -18,33 +18,41 @@ require_once(dirname(__FILE__) . '/learningresources.class.php');
 
 class block_learningresources extends block_base {
 
+    // title for the block on the course page
     public function init() {
         $this->title = get_string('learningresources', 'block_learningresources');
     }
         
+    // content for the block on the course page
     public function get_content() {
 
+        // get the defaults
         $default_lr_array = new lr_list();
-//        echo "<pre>Debug: ";
-//        print_r($default_lr_array);
-//        echo "</pre>";
-//        die();
 
+        // and get the instance settings
         $config = $this->config;
+        // if there are instance settings
         if ($config) {
+            // go through the list
             foreach ($config as $key => $value) {
+                // if the setting corresponds to an item in the default list
                 if (array_key_exists($key, $default_lr_array->lr_array)) {
+                    // set the visibility in the list according to the instance preference
                     $default_lr_array->set_visibility($key, $value);
                 }
             }
         }
 
+        // create the object to hold the content
         $this->content = new stdClass;
+
+        // assign the HTML list (with instance preferences) as the text of the content
         $this->content->text = $default_lr_array->get_html_list();
 
         return $this->content;
     }
 
+    // tells Moodle to look for global settings
     public function has_config() {
         return true;
     }
