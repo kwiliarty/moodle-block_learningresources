2b618899138995fb180811b9708ab20773e98974
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2013-07-16 06:26:31 -0400

    commenting the edit_form

--- edit_form.php
+++ edit_form.php
@@ -2,25 +2,32 @@
 
 require_once(dirname(__FILE__) . '/learningresources.class.php');
 
-class block_learningresources_edit_form extends block_edit_form {
+class block_learningresources_edit_form extends block_edit_form { /* creates the instance preferences form */
 
     protected function specific_definition($mform) {
 
-        $lr_resources = new lr_list();
-        $lr_array = $lr_resources->get_lr_array();
+        $lr_resources = new lr_list(); /* get the default list */
 
+        $lr_array = $lr_resources->get_lr_array(); /* variable to hold the resources array */
+
+        // title for the checkbox section
         $mform->addElement('header', 'configheader', get_string('blocksettings', 'block_learningresources'));
 
+        // loop through the default array
         foreach ($lr_array as $lr) {
+            // format a link to the resource
             $anchor = "<a href='" . $lr['url'] . "' target='_blank' >" . $lr['text'] . "</a>";
+            // variable to hold the specific fieldname
             $fieldname = "config_" . $lr['id'];
-            $mform->addElement('advcheckbox',
+            // add a checkbox
+            // Moodle will apply the preferences if they have been set
+            $mform->addElement('advcheckbox', 
                                $fieldname,
-                               $lr['id'],
+                               $lr['id'], /* displays before the checkbox */
                                $anchor,
                                null,
-                               array('hide', 'show'));
-            $mform->setDefault($fieldname, $lr['show']);
+                               array('hide', 'show')); /* assigns values to "off" and "on" */
+            $mform->setDefault($fieldname, $lr['show']); /* use defaults if no instance config */
         }
     }
 }
