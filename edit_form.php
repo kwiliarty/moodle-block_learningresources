4f985a1a91cebc7fd235f58e2030f690845453e2
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2013-07-17 05:57:21 -0400

    comments for edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -2,27 +2,37 @@
 
 require_once(dirname(__FILE__) . '/learningresources.class.php');
 
-class block_learningresources_edit_form extends block_edit_form { /* creates the instance preferences form */
+/* create the instance preferences form */
+class block_learningresources_edit_form extends block_edit_form { 
 
     protected function specific_definition($mform) {
 
-        $lr_resources = new lr_list(); /* get the default list */
+        /* get the list of default items
+         * create variable to hold the nested array of items and attributes
+         */
+        $lr_resources = new lr_list();
+        $lr_array = $lr_resources->get_lr_array();
 
-        $lr_array = $lr_resources->get_lr_array(); /* variable to hold the resources array */
-
-        // title for the checkbox section
+        /* title for the checkbox section
+         */
         $mform->addElement('header', 'configheader', get_string('blocksettings', 'block_learningresources'));
 
-        foreach ($lr_array as $lr) { /* loop through the default resources */
-            $anchor = html_writer::link($lr['url'], $lr['text'], array('target'=>'_blank')); /* link to resource */
-            $fieldname = "config_" . $lr['id']; /* fieldname for checkbox */
-            $mform->addElement('advcheckbox', /* moodle applies preferences if they have been set already */
+        /* loop through the list of default resources
+         * format a link to each resource
+         * format a fieldname for a corresponding checkbox, must begin with "config_"
+         * Moodle will apply instance preferences if they have been set
+         * Otherwise use the visibility preference for the item from the default list
+         */
+        foreach ($lr_array as $lr) {
+            $anchor = html_writer::link($lr['url'], $lr['text'], array('target'=>'_blank'));
+            $fieldname = "config_" . $lr['id'];
+            $mform->addElement('advcheckbox', /* allow unchecked box to send a value */
                                $fieldname,
                                $lr['id'], /* displays before the checkbox */
                                $anchor, /* displays after checkbox */
                                null, /* potential group for checkbox controller */
                                array('hide', 'show')); /* assigns values to "off" and "on" */
-            $mform->setDefault($fieldname, $lr['show']); /* use defaults if no instance config */
+            $mform->setDefault($fieldname, $lr['show']);
         }
     }
 }
